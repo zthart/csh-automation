7066ab23c6f4f8f92a6fb0a39c82d35e12b92c98
Zach Hart <swolf387@gmail.com>
2016-02-15 23:40:31 +0000

    Added .service file and used libcec

--- receiverControl.py
+++ receiverControl.py
@@ -18,6 +18,150 @@ inputs = {1:(inputa, "1. Sets the active input on the receiver to the Media PC")
 3:(inputc, "3. Sets the active input on the receiver to the Chromecast")}
 #4:(inputd, "4. Sets the active input on the receiver to this machine - debug only")
 
+import cec
+print(cec)
+
+class pyCecClient:
+  cecconfig = cec.libcec_configuration()
+  lib = {}
+  # don't enable debug logging by default
+  log_level = cec.CEC_LOG_TRAFFIC
+
+  # create a new libcec_configuration
+  def SetConfiguration(self):
+    self.cecconfig.strDeviceName   = "pyLibCec"
+    self.cecconfig.bActivateSource = 0
+    self.cecconfig.deviceTypes.Add(cec.CEC_DEVICE_TYPE_RECORDING_DEVICE)
+    self.cecconfig.clientVersion = cec.LIBCEC_VERSION_CURRENT
+
+  def SetLogCallback(self, callback):
+    self.cecconfig.SetLogCallback(callback)
+
+  def SetKeyPressCallback(self, callback):
+    self.cecconfig.SetKeyPressCallback(callback)
+
+  # detect an adapter and return the com port path
+  def DetectAdapter(self):
+    retval = None
+    adapters = self.lib.DetectAdapters()
+    for adapter in adapters:
+      print("found a CEC adapter:")
+      print("port:     " + adapter.strComName)
+      print("vendor:   " + hex(adapter.iVendorId))
+      print("product:  " + hex(adapter.iProductId))
+      retval = adapter.strComName
+    return retval
+
+  # initialise libCEC
+  def InitLibCec(self):
+    self.lib = cec.ICECAdapter.Create(self.cecconfig)
+    # print libCEC version and compilation information
+    print("libCEC version " + self.lib.VersionToString(self.cecconfig.serverVersion) + " loaded: " + self.lib.GetLibInfo())
+
+    # search for adapters
+    adapter = self.DetectAdapter()
+    if adapter == None:
+      print("No adapters found")
+    else:
+      if self.lib.Open(adapter):
+        print("connection opened")
+      else:
+        print("failed to open a connection to the CEC adapter")
+
+  # display the addresses controlled by libCEC
+  def ProcessCommandSelf(self):
+    addresses = self.lib.GetLogicalAddresses()
+    strOut = "Addresses controlled by libCEC: "
+    x = 0
+    notFirst = False
+    while x < 15:
+      if addresses.IsSet(x):
+        if notFirst:
+          strOut += ", "
+        strOut += self.lib.LogicalAddressToString(x)
+        if self.lib.IsActiveSource(x):
+          strOut += " (*)"
+        notFirst = True
+      x += 1
+    print(strOut)
+
+  # send an active source message
+  def ProcessCommandActiveSource(self):
+    self.lib.SetActiveSource()
+
+  # send a standby command
+  def ProcessCommandStandby(self):
+    self.lib.StandbyDevices(CECDEVICE_BROADCAST)
+
+  # send a custom command
+  def ProcessCommandTx(self, data):
+    cmd = self.lib.CommandFromString(data)
+    print("transmit " + data)
+    if self.lib.Transmit(cmd):
+      print("command sent")
+    else:
+      print("failed to send command")
+
+  # scan the bus and display devices that were found
+  def ProcessCommandScan(self):
+    print("requesting CEC bus information ...")
+    strLog = "CEC bus information\n===================\n"
+    addresses = self.lib.GetActiveDevices()
+    activeSource = self.lib.GetActiveSource()
+    x = 0
+    while x < 15:
+      if addresses.IsSet(x):
+        vendorId        = self.lib.GetDeviceVendorId(x)
+        physicalAddress = self.lib.GetDevicePhysicalAddress(x)
+        active          = self.lib.IsActiveSource(x)
+        cecVersion      = self.lib.GetDeviceCecVersion(x)
+        power           = self.lib.GetDevicePowerStatus(x)
+        osdName         = self.lib.GetDeviceOSDName(x)
+        strLog += "device #" + str(x) +": " + self.lib.LogicalAddressToString(x)  + "\n"
+        strLog += "address:       " + str(physicalAddress) + "\n"
+        strLog += "active source: " + str(active) + "\n"
+        strLog += "vendor:        " + self.lib.VendorIdToString(vendorId) + "\n"
+        strLog += "CEC version:   " + self.lib.CecVersionToString(cecVersion) + "\n"
+        strLog += "OSD name:      " + osdName + "\n"
+        strLog += "power status:  " + self.lib.PowerStatusToString(power) + "\n\n\n"
+      x += 1
+    print(strLog)
+
+  # logging callback
+  def LogCallback(self, level, time, message):
+    if level > self.log_level:
+      return 0
+
+    if level == cec.CEC_LOG_ERROR:
+      levelstr = "ERROR:   "
+    elif level == cec.CEC_LOG_WARNING:
+      levelstr = "WARNING: "
+    elif level == cec.CEC_LOG_NOTICE:
+      levelstr = "NOTICE:  "
+    elif level == cec.CEC_LOG_TRAFFIC:
+      levelstr = "TRAFFIC: "
+    elif level == cec.CEC_LOG_DEBUG:
+      levelstr = "DEBUG:   "
+
+    print(levelstr + "[" + str(time) + "]     " + message)
+    return 0
+
+  # key press callback
+  def KeyPressCallback(self, key, duration):
+    print("[key pressed] " + str(key))
+    return 0
+
+  def __init__(self):
+    self.SetConfiguration()
+
+# logging callback
+def log_callback(level, time, message):
+  return lib.LogCallback(level, time, message)
+
+# key press callback
+def key_press_callback(key, duration):
+  return lib.KeyPressCallback(key, duration)
+
 ser = serial.Serial(
 	port='/dev/ttyUSB0',
 	baudrate=115200,
@@ -27,6 +171,13 @@ ser = serial.Serial(
 )
 
 ser.isOpen()
+
+lib = pyCecClient()
+lib.SetLogCallback(log_callback)
+lib.SetKeyPressCallback(key_press_callback)
+
+# initialise libCEC and enter the main loop
+lib.InitLibCec()
 
 @app.route('/input/<int:id>')
 def lounge_input(id):
@@ -58,8 +209,8 @@ def lounge_vol_mute():
 	#These commands are being sent as button presses
 	#I think mute doesn't actually apply until button release
 	#And I can't send two commands before closing the client
-	os.system("echo \"tx 14:44:43\" | cec-client -s")
-	#os.system("echo \"tx 15:45\" | cec-client -s")
+	lib.ProcessCommandTx("15:44:43")
+	lib.ProcessCommandTx("15:45")
 	return make_response("okay", 200)
 
 @app.route('/proj/on')
@@ -79,4 +230,5 @@ def lounge_proj_status():
 
 
 if __name__ == '__main__':
+  # initialise libCEC
 	app.run(host='0.0.0.0', debug=True)
