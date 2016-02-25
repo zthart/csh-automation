2e25db3918649cd3066caca92ee2a4cc6eeb65e9
Zach Hart <swolf387@gmail.com>
2016-02-25 22:32:29 +0000

    changed to allow for individual control via requests

--- lightingControl.py
+++ lightingControl.py
@@ -13,23 +13,30 @@ token = "<API KEY>"
 
 @app.route('/lounge/lights', methods=["GET","PUT"])
 def lounge_lights():
-	req = request.get_json()
-	if req["token"]["id"] == token:
-		if req["lights"]["L1"] == True:
-			os.system("heyu on L1")
-		elif req["lights"]["L1"] == False:
-			os.system("heyu off L1")
-		else:
-			return make_response(jsonify({"status" : {"success" : False}}), 412)
-		if req["lights"]["L2"] == True:
-			os.system("heyu on L2")
-		elif req["lights"]["L2"] == False:
-			os.system("heyu off L2")
-		else:
-			return make_response(jsonify({"status" : {"success" : False}}), 412)
-		return make_response(jsonify({"status" : {"success" : True}}), 200)
-	else:
-		return make_response(jsonify({"status" : {"success" : False}}), 400)
+    req = request.get_json()
+    if req["token"]["id"] == token:
+        for light, value in req["lights"].items():
+            print(light, value)
+
+            if light == "L1":
+                if value == True:
+                    os.system("heyu on L1");
+                elif value == False:
+                    os.system("heyu off L1");
+                else:
+                    return make_response(jsonify({"status" : {"success" : False}}), 412)
+            elif light == "L2":
+                if value == True:
+                    os.system("heyu on L2");
+                elif value == False:
+                    os.system("heyu off L2");
+                else:
+                    return make_response(jsonify({"status" : {"success" : False}}), 412)
+            else:
+                return make_response(jsonify({"status" : {"success" : False}}), 400)
+        return make_response(jsonify({"status" : {"success" : True}}), 200)
+    else:
+        return make_response(jsonify({"status" : {"success" : False}}), 400)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
