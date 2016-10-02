b10d3c7878a8d7e55dbca1705fd1765ddd7aed0b
Zach Hart <swolf387@gmail.com>
2016-10-01 21:12:14 -0400

    cleaned code for ease in reading

--- receiverControl.py
+++ receiverControl.py
@@ -19,159 +19,159 @@ import cec
 print(cec)
 
 class pyCecClient:
-  cecconfig = cec.libcec_configuration()
-  lib = {}
-  # don't enable debug logging by default
-  log_level = cec.CEC_LOG_TRAFFIC
+    cecconfig = cec.libcec_configuration()
+    lib = {}
+    # don't enable debug logging by default
+    log_level = cec.CEC_LOG_TRAFFIC
 
-  # create a new libcec_configuration
-  def SetConfiguration(self):
-    self.cecconfig.strDeviceName   = "pyLibCec"
-    self.cecconfig.bActivateSource = 0
-    self.cecconfig.deviceTypes.Add(cec.CEC_DEVICE_TYPE_RECORDING_DEVICE)
-    self.cecconfig.clientVersion = cec.LIBCEC_VERSION_CURRENT
+    # create a new libcec_configuration
+    def SetConfiguration(self):
+        self.cecconfig.strDeviceName   = "pyLibCec"
+        self.cecconfig.bActivateSource = 0
+        self.cecconfig.deviceTypes.Add(cec.CEC_DEVICE_TYPE_RECORDING_DEVICE)
+        self.cecconfig.clientVersion = cec.LIBCEC_VERSION_CURRENT
 
-  def SetLogCallback(self, callback):
-    self.cecconfig.SetLogCallback(callback)
+    def SetLogCallback(self, callback):
+        self.cecconfig.SetLogCallback(callback)
 
-  def SetKeyPressCallback(self, callback):
-    self.cecconfig.SetKeyPressCallback(callback)
+    def SetKeyPressCallback(self, callback):
+        self.cecconfig.SetKeyPressCallback(callback)
 
-  # detect an adapter and return the com port path
-  def DetectAdapter(self):
-    retval = None
-    adapters = self.lib.DetectAdapters()
-    for adapter in adapters:
-      print("found a CEC adapter:")
-      print("port:     " + adapter.strComName)
-      print("vendor:   " + hex(adapter.iVendorId))
-      print("product:  " + hex(adapter.iProductId))
-      retval = adapter.strComName
-    return retval
+    # detect an adapter and return the com port path
+    def DetectAdapter(self):
+        retval = None
+        adapters = self.lib.DetectAdapters()
+        for adapter in adapters:
+            print("found a CEC adapter:")
+            print("port:     " + adapter.strComName)
+            print("vendor:   " + hex(adapter.iVendorId))
+            print("product:  " + hex(adapter.iProductId))
+            retval = adapter.strComName
+        return retval
 
-  # initialise libCEC
-  def InitLibCec(self):
-    self.lib = cec.ICECAdapter.Create(self.cecconfig)
-    # print libCEC version and compilation information
-    print("libCEC version " + self.lib.VersionToString(self.cecconfig.serverVersion) + " loaded: " + self.lib.GetLibInfo())
+    # initialise libCEC
+    def InitLibCec(self):
+        self.lib = cec.ICECAdapter.Create(self.cecconfig)
+        # print libCEC version and compilation information
+        print("libCEC version " + self.lib.VersionToString(self.cecconfig.serverVersion) + " loaded: " + self.lib.GetLibInfo())
 
-    # search for adapters
-    adapter = self.DetectAdapter()
-    if adapter == None:
-      print("No adapters found")
-    else:
-      if self.lib.Open(adapter):
-        print("connection opened")
-      else:
-        print("failed to open a connection to the CEC adapter")
+        # search for adapters
+        adapter = self.DetectAdapter()
+        if adapter == None:
+            print("No adapters found")
+        else:
+            if self.lib.Open(adapter):
+                print("connection opened")
+            else:
+                print("failed to open a connection to the CEC adapter")
 
-  # display the addresses controlled by libCEC
-  def ProcessCommandSelf(self):
-    addresses = self.lib.GetLogicalAddresses()
-    strOut = "Addresses controlled by libCEC: "
-    x = 0
-    notFirst = False
-    while x < 15:
-      if addresses.IsSet(x):
-        if notFirst:
-          strOut += ", "
-        strOut += self.lib.LogicalAddressToString(x)
-        if self.lib.IsActiveSource(x):
-          strOut += " (*)"
-        notFirst = True
-      x += 1
-    print(strOut)
+    # display the addresses controlled by libCEC
+    def ProcessCommandSelf(self):
+        addresses = self.lib.GetLogicalAddresses()
+        strOut = "Addresses controlled by libCEC: "
+        x = 0
+        notFirst = False
+        while x < 15:
+            if addresses.IsSet(x):
+                if notFirst:
+                    strOut += ", "
+                strOut += self.lib.LogicalAddressToString(x)
+                if self.lib.IsActiveSource(x):
+                    strOut += " (*)"
+                notFirst = True
+            x += 1
+        print(strOut)
 
-  # send an active source message
-  def ProcessCommandActiveSource(self):
-    self.lib.SetActiveSource()
+    # send an active source message
+    def ProcessCommandActiveSource(self):
+        self.lib.SetActiveSource()
 
-  # send a standby command
-  def ProcessCommandStandby(self):
-    self.lib.StandbyDevices(CECDEVICE_BROADCAST)
+    # send a standby command
+    def ProcessCommandStandby(self):
+        self.lib.StandbyDevices(CECDEVICE_BROADCAST)
 
-  # send a custom command
-  def ProcessCommandTx(self, data):
-    cmd = self.lib.CommandFromString(data)
-    print("transmit " + data)
-    if self.lib.Transmit(cmd):
-      print("command sent")
-    else:
-      print("failed to send command")
+    # send a custom command
+    def ProcessCommandTx(self, data):
+        cmd = self.lib.CommandFromString(data)
+        print("transmit " + data)
+        if self.lib.Transmit(cmd):
+            print("command sent")
+        else:
+            print("failed to send command")
 
-  # scan the bus and display devices that were found
-  def ProcessCommandScan(self):
-    print("requesting CEC bus information ...")
-    strLog = "CEC bus information\n===================\n"
-    addresses = self.lib.GetActiveDevices()
-    activeSource = self.lib.GetActiveSource()
-    x = 0
-    while x < 15:
-      if addresses.IsSet(x):
-        vendorId        = self.lib.GetDeviceVendorId(x)
-        physicalAddress = self.lib.GetDevicePhysicalAddress(x)
-        active          = self.lib.IsActiveSource(x)
-        cecVersion      = self.lib.GetDeviceCecVersion(x)
-        power           = self.lib.GetDevicePowerStatus(x)
-        osdName         = self.lib.GetDeviceOSDName(x)
-        strLog += "device #" + str(x) +": " + self.lib.LogicalAddressToString(x)  + "\n"
-        strLog += "address:       " + str(physicalAddress) + "\n"
-        strLog += "active source: " + str(active) + "\n"
-        strLog += "vendor:        " + self.lib.VendorIdToString(vendorId) + "\n"
-        strLog += "CEC version:   " + self.lib.CecVersionToString(cecVersion) + "\n"
-        strLog += "OSD name:      " + osdName + "\n"
-        strLog += "power status:  " + self.lib.PowerStatusToString(power) + "\n\n\n"
-      x += 1
-    print(strLog)
+    # scan the bus and display devices that were found
+    def ProcessCommandScan(self):
+        print("requesting CEC bus information ...")
+        strLog = "CEC bus information\n===================\n"
+        addresses = self.lib.GetActiveDevices()
+        activeSource = self.lib.GetActiveSource()
+        x = 0
+        while x < 15:
+            if addresses.IsSet(x):
+                vendorId        = self.lib.GetDeviceVendorId(x)
+                physicalAddress = self.lib.GetDevicePhysicalAddress(x)
+                active          = self.lib.IsActiveSource(x)
+                cecVersion      = self.lib.GetDeviceCecVersion(x)
+                power           = self.lib.GetDevicePowerStatus(x)
+                osdName         = self.lib.GetDeviceOSDName(x)
+                strLog += "device #" + str(x) +": " + self.lib.LogicalAddressToString(x)  + "\n"
+                strLog += "address:       " + str(physicalAddress) + "\n"
+                strLog += "active source: " + str(active) + "\n"
+                strLog += "vendor:        " + self.lib.VendorIdToString(vendorId) + "\n"
+                strLog += "CEC version:   " + self.lib.CecVersionToString(cecVersion) + "\n"
+                strLog += "OSD name:      " + osdName + "\n"
+                strLog += "power status:  " + self.lib.PowerStatusToString(power) + "\n\n\n"
+            x += 1
+        print(strLog)
 
-  # logging callback
-  def LogCallback(self, level, time, message):
-    global sourceChangeCallback
-    if level > self.log_level:
-      return 0
+    # logging callback
+    def LogCallback(self, level, time, message):
+        global sourceChangeCallback
+        if level > self.log_level:
+            return 0
 
-    if level == cec.CEC_LOG_ERROR:
-      levelstr = "ERROR:   "
-    elif level == cec.CEC_LOG_WARNING:
-      levelstr = "WARNING: "
-    elif level == cec.CEC_LOG_NOTICE:
-      levelstr = "NOTICE:  "
-    elif level == cec.CEC_LOG_TRAFFIC:
-      levelstr = "TRAFFIC: "
-      #If the message is a broadcast from the audio system
-      #specifying a routing change, store it in sourceChangeCallback
-      if message.find("5f:80") > 0:
-          sourceChangeCallback = message
-          lounge_sourceChanged()
-    elif level == cec.CEC_LOG_DEBUG:
-      levelstr = "DEBUG:   "
+        if level == cec.CEC_LOG_ERROR:
+            levelstr = "ERROR:   "
+        elif level == cec.CEC_LOG_WARNING:
+            levelstr = "WARNING: "
+        elif level == cec.CEC_LOG_NOTICE:
+            levelstr = "NOTICE:  "
+        elif level == cec.CEC_LOG_TRAFFIC:
+            levelstr = "TRAFFIC: "
+            #If the message is a broadcast from the audio system
+            #specifying a routing change, store it in sourceChangeCallback
+            if message.find("5f:80") > 0:
+                sourceChangeCallback = message
+                lounge_sourceChanged()
+        elif level == cec.CEC_LOG_DEBUG:
+            levelstr = "DEBUG:   "
 
-    print(levelstr + "[" + str(time) + "]     " + message)
-    return 0
+        print(levelstr + "[" + str(time) + "]     " + message)
+        return 0
 
-  # key press callback
-  def KeyPressCallback(self, key, duration):
-    print("[key pressed] " + str(key))
-    return 0
+    # key press callback
+    def KeyPressCallback(self, key, duration):
+        print("[key pressed] " + str(key))
+        return 0
 
-  def __init__(self):
-    self.SetConfiguration()
+    def __init__(self):
+        self.SetConfiguration()
 
 # logging callback
 def log_callback(level, time, message):
-  return lib.LogCallback(level, time, message)
+    return lib.LogCallback(level, time, message)
 
 # key press callback
 def key_press_callback(key, duration):
-  return lib.KeyPressCallback(key, duration)
+    return lib.KeyPressCallback(key, duration)
 
 #Set up serial communications with the projector
 ser = serial.Serial(
-	port='/dev/ttyUSB0',
-	baudrate=115200,
-	parity=serial.PARITY_NONE,
-	stopbits=serial.STOPBITS_ONE,
-	bytesize=serial.EIGHTBITS
+    port='/dev/ttyUSB0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
 )
 
 if not ser.isOpen():
@@ -179,7 +179,7 @@ if not ser.isOpen():
 else:
     ser.close()
 
-#LibCEC stuff, should be eventually culled back to what we actually need
+#Initialize the CEC client, set callbacks properly
 lib = pyCecClient()
 lib.SetLogCallback(log_callback)
 lib.SetKeyPressCallback(key_press_callback)
@@ -194,35 +194,35 @@ lib.InitLibCec()
 
 @app.route('/lounge/receiver/input', methods=["GET", "PUT"])
 def lounge_input():
-        global lastKnownSource
-	#store request json
-	req = request.get_json()
-	input = req["input"]["select"]
-	if req["token"]["id"] == token:
-		if input == "HDMI1":
-			#Change to MediaPC (2.1.0.0)
-                        lastKnownSource = "21:00"
-			lib.ProcessCommandTx("1f:82:21:00")
-		elif input == "HDMI2":
-			#Change to AuxHDMI (2.2.0.0)
-                        lastKnownSource = "22:00"
-			lib.ProcessCommandTx("1f:82:22:00")
-		elif input == "HDMI3":
-			#Change to Chromecast (2.3.0.0)
-                        lastKnownSource = "23:00"
-			lib.ProcessCommandTx("1f:82:23:00")
-		elif input == "HDMI4":
-			#Change to Chromecast (2.4.0.0)
-                        lastKnownSource = "24:00"
-			lib.ProcessCommandTx("1f:82:24:00")
-		else:
-			#If no proper input was selected
-			return make_response(jsonify({"status" : {"success":False}}), 400)
-		#If an available input was selected and switched
-		return make_response(jsonify({"status" : {"success":True}}), 200)
-	else:
-		#If the provided token was incorrect
-		return make_response(jsonify({"status" : {"success":False}}), 400)
+    global lastKnownSource
+    #store request json
+    req = request.get_json()
+    input = req["input"]["select"]
+    if req["token"]["id"] == token:
+        if input == "HDMI1":
+            #Change to MediaPC (2.1.0.0)
+            lastKnownSource = "21:00"
+            lib.ProcessCommandTx("1f:82:21:00")
+        elif input == "HDMI2":
+            #Change to AuxHDMI (2.2.0.0)
+            lastKnownSource = "22:00"
+            lib.ProcessCommandTx("1f:82:22:00")
+        elif input == "HDMI3":
+            #Change to Chromecast (2.3.0.0)
+            lastKnownSource = "23:00"
+            lib.ProcessCommandTx("1f:82:23:00")
+        elif input == "HDMI4":
+            #Change to Chromecast (2.4.0.0)
+            lastKnownSource = "24:00"
+            lib.ProcessCommandTx("1f:82:24:00")
+        else:
+            #If no proper input was selected
+            return make_response(jsonify({"status" : {"success":False}}), 400)
+        #If an available input was selected and switched
+        return make_response(jsonify({"status" : {"success":True}}), 200)
+    else:
+        #If the provided token was incorrect
+        return make_response(jsonify({"status" : {"success":False}}), 400)
 
 #Mute Control
 #
@@ -242,29 +242,29 @@ def lounge_input():
 
 @app.route('/lounge/receiver/mute', methods=["GET", "PUT"])
 def lounge_mute():
-	#store request json
-	req = request.get_json()
-	if req["token"]["id"] == token:
-            if req["mute"]["state"] == True:
-                if lounge_audio_status() > 127:
-                    return make_response(jsonify({"status" : {"success":True}}), 200)
-                else:
-                    lib.ProcessCommandTx("15:44:43")
-                    lib.ProcessCommandTx("15:45")
-                    lib.ProcessCommandTx("15:71")
-                    return make_response(jsonify({"status" : {"success":True}}), 200)
-            elif req["mute"]["state"] == False:
-                if lounge_audio_status() > 127:
-                    lib.ProcessCommandTx("15:44:43")
-                    lib.ProcessCommandTx("15:45")
-                    lib.ProcessCommandTx("15:71")
-                    return make_response(jsonify({"status" : {"success":True}}), 200)
-                else:
-                    return make_response(jsonify({"status" : {"success":True}}), 200)
+    #store request json
+    req = request.get_json()
+    if req["token"]["id"] == token:
+        if req["mute"]["state"] == True:
+            if lounge_audio_status() > 127:
+                return make_response(jsonify({"status" : {"success":True}}), 200)
             else:
-                return make_response(jsonify({"status" : {"success":False}}), 400)
+                lib.ProcessCommandTx("15:44:43")
+                lib.ProcessCommandTx("15:45")
+                lib.ProcessCommandTx("15:71")
+                return make_response(jsonify({"status" : {"success":True}}), 200)
+        elif req["mute"]["state"] == False:
+            if lounge_audio_status() > 127:
+                lib.ProcessCommandTx("15:44:43")
+                lib.ProcessCommandTx("15:45")
+                lib.ProcessCommandTx("15:71")
+                return make_response(jsonify({"status" : {"success":True}}), 200)
+            else:
+                return make_response(jsonify({"status" : {"success":True}}), 200)
         else:
             return make_response(jsonify({"status" : {"success":False}}), 400)
+    else:
+        return make_response(jsonify({"status" : {"success":False}}), 400)
 
 #Volume Control
 #
@@ -278,30 +278,30 @@ def lounge_mute():
 
 @app.route('/lounge/receiver/volume', methods=["GET", "PUT"])
 def lounge_volume():
-	req = request.get_json()
-        if req["token"]["id"] == token:
-            level = req["volume"]["level"]
-            if level > 101 and lounge_audio_status() > 101:
-                return make_response(jsonify({"status" : {"success":False}}), 200)
-            elif level > 101 and lounge_audio_status() < 101:
-                level = 100
+    req = request.get_json()
+    if req["token"]["id"] == token:
+        level = req["volume"]["level"]
+        if level > 101 and lounge_audio_status() > 101:
+            return make_response(jsonify({"status" : {"success":False}}), 200)
+        elif level > 101 and lounge_audio_status() < 101:
+            level = 100
 
-            if level < lounge_audio_status():
-                while abs(level-lounge_audio_status()) > 3:
-                    lib.ProcessCommandTx("15:44:42")
-                    lib.ProcessCommandTx("15:45")
-                    lib.ProcessCommandTx("15:71")
-                return make_response(jsonify({"status" : {"success":True}}), 200)
-            elif level > lounge_audio_status():
-                while abs(level-lounge_audio_status()) > 3:
-                    lib.ProcessCommandTx("15:44:41")
-                    lib.ProcessCommandTx("15:45")
-                    lib.ProcessCommandTx("15:71")
-                return make_response(jsonify({"status" : {"success":True}}), 200)
-            else:
-                return make_response(jsonify({"stauts" : {"success":True}}), 200)
+        if level < lounge_audio_status():
+            while abs(level-lounge_audio_status()) > 3:
+                lib.ProcessCommandTx("15:44:42")
+                lib.ProcessCommandTx("15:45")
+                lib.ProcessCommandTx("15:71")
+            return make_response(jsonify({"status" : {"success":True}}), 200)
+        elif level > lounge_audio_status():
+            while abs(level-lounge_audio_status()) > 3:
+                lib.ProcessCommandTx("15:44:41")
+                lib.ProcessCommandTx("15:45")
+                lib.ProcessCommandTx("15:71")
+            return make_response(jsonify({"status" : {"success":True}}), 200)
         else:
-            return make_response(jsonify({"status" : {"success":False}}), 400)
+            return make_response(jsonify({"stauts" : {"success":True}}), 200)
+    else:
+        return make_response(jsonify({"status" : {"success":False}}), 400)
 
 #Refresh Audio Status
 #
@@ -309,9 +309,9 @@ def lounge_volume():
 #for its response. Returns a value between 1-127 for unmuted volumes,
 #returns the previous audio value plus 128 for muted volumes.
 def lounge_audio_status():
-	lib.ProcessCommandTx("15:71")
-	audio_status = lib.lib.AudioStatus()
-	return audio_status
+    lib.ProcessCommandTx("15:71")
+    audio_status = lib.lib.AudioStatus()
+    return audio_status
 
 #Refresh Input Status
 #
@@ -320,13 +320,13 @@ def lounge_audio_status():
 #means to keep track of changes to the receiver's source via the front
 #panel on the receiver.
 def lounge_sourceChanged():
-        global lastKnownSource
-        global sourceChangeCallback
-        if lastKnownSource != sourceChangeCallback[15:]:
-            lastKnownSource = sourceChangeCallback[15:]
-            return True
-        else:
-            return False
+    global lastKnownSource
+    global sourceChangeCallback
+    if lastKnownSource != sourceChangeCallback[15:]:
+        lastKnownSource = sourceChangeCallback[15:]
+        return True
+    else:
+        return False
 
 #Receiver Status
 #
